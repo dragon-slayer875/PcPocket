bc31834276a535a6819313357e99e43497af6890
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-03-14 14:05:28 +0530

    chore: initialize tanstack query
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
 import { routeTree } from "@/routeTree.gen";
+
 import { ThemeProvider } from "./components/themeProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
+
+// Create a new query client instance
+const queryClient = new QueryClient();
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -23,7 +29,10 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </ThemeProvider>
     </StrictMode>,
   );
