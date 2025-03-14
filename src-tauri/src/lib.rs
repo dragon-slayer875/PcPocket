441cacdd3c95c2f106dff2dca1bed81866d7bb7b
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-03-15 00:48:38 +0530

    chore: define migrations for internal database
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,8 +8,34 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![
+        // Define your migrations here
+        Migration {
+            version: 1,
+            description: "create_initial_tables",
+            sql: "CREATE TABLE bookmarks_table (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  link TEXT NOT NULL,
+  icon_link TEXT,
+  created_at INTEGER NOT NULL DEFAULT (strftime('%s','now'))
+);
+            CREATE TABLE tags_table (
+  bookmark_id INTEGER NOT NULL,
+  tag_name TEXT NOT NULL,
+  PRIMARY KEY (bookmark_id, tag_name),
+  FOREIGN KEY (bookmark_id) REFERENCES bookmarks_table(id) ON DELETE CASCADE
+);",
+            kind: MigrationKind::Up,
+        },
+    ];
+
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:bookmarks.tmp", migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_persisted_scope::init())
