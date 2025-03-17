98cfcc093b29bf7a8acf9215c0ac3ec4ad8206e7
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-03-17 17:39:52 +0530

    code: add ability to customize back button behavior
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
         storeFile = file(keystoreProperties["storeFile"] as String)
         storePassword = keystoreProperties["password"] as String
     }
+    }
     buildTypes {
         getByName("debug") {
             manifestPlaceholders["usesCleartextTraffic"] = "true"
