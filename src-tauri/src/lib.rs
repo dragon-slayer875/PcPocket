913dbcdc42bd338cb58e01b4f4b36c718f0056a3
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-03-17 23:29:14 +0530

    code: add tauri notification plugin
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -66,6 +66,7 @@ pub fn run() {
     ];
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
