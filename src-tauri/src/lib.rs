106df473191b8f9ca50fad329a899451ba743165
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-03-20 21:15:21 +0530

    code: add global shortcut plugin for tauri
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -73,6 +73,7 @@ pub fn run() {
     ];
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(
