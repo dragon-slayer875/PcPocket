06a6574b69b3719d2a282655f60c678189ef62e6
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-03-24 02:23:57 +0530

    feat: add ability to open existing db
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -20,11 +20,12 @@ import { Route } from "@/routes/main";
 // import { sendNotification } from "@tauri-apps/plugin-notification";
 // import { join } from "@tauri-apps/api/path";
 import { getLinkPreview } from "link-preview-js";
+import { BaseDirectory, copyFile } from "@tauri-apps/plugin-fs";
 
 export function useGetDbPathQuery() {
   return useQuery({
     queryKey: ["dbPath"],
-    queryFn: async function () {
+    queryFn: async function() {
       return accessStore("get", "dbPath");
     },
   });
@@ -34,8 +35,10 @@ export function useCreateDbMutation() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: async function () {
-      return save();
+    mutationFn: async function() {
+      return save({
+        defaultPath: "bookmarks.db",
+      });
     },
     async onSuccess(dbPath) {
       await accessStore("set", "dbPath", dbPath);
@@ -51,11 +54,14 @@ export function useCreateDbMutation() {
 export function useOpenDbMutation() {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: async function () {
+    mutationFn: async function() {
       return open();
     },
     async onSuccess(dbPath) {
       await accessStore("set", "dbPath", dbPath);
+      await copyFile(dbPath!, "bookmarks.tmp", {
+        toPathBaseDir: BaseDirectory.AppConfig,
+      });
       queryClient.invalidateQueries({ queryKey: ["dbPath"] });
     },
   });
@@ -67,7 +73,7 @@ export function useGetBookmarksQuery(
 ) {
   return useQuery({
     queryKey: ["bookmarks", pageSize, cursor],
-    queryFn: async function () {
+    queryFn: async function() {
       const bookmarks: BookmarkQueryItem[] = await getBookmarks();
       return bookmarks;
     },
@@ -77,7 +83,7 @@ export function useGetBookmarksQuery(
 export function useGetMetadataQuery(url: string) {
   return useQuery({
     queryKey: ["metadata", url],
-    queryFn: async function () {
+    queryFn: async function() {
       return getLinkPreview(url);
     },
   });
@@ -86,7 +92,7 @@ export function useGetMetadataQuery(url: string) {
 export function useImportBookmarksMutation() {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: async function () {
+    mutationFn: async function() {
       const path = await open();
       if (path) await importBookmarks(path);
     },
@@ -140,7 +146,7 @@ export function useImportBookmarksMutation() {
 export function useInsertBookmarkMutation() {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: async function (bookmark: BookmarkMutationItem) {
+    mutationFn: async function(bookmark: BookmarkMutationItem) {
       await insertBookmark(bookmark);
     },
     onSuccess() {
@@ -152,7 +158,7 @@ export function useInsertBookmarkMutation() {
 export function useUpdateBookmarkMutation() {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: async function (bookmark: BookmarkMutationItem) {
+    mutationFn: async function(bookmark: BookmarkMutationItem) {
       await updateBookmark(bookmark);
     },
     onSuccess() {
@@ -164,7 +170,7 @@ export function useUpdateBookmarkMutation() {
 export function useDeleteBookmarkMutation() {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: async function (id: number) {
+    mutationFn: async function(id: number) {
       await deleteBookmark(id);
     },
     onSuccess() {
