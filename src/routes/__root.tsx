46363d867b2e4949e76cbe07f8aaf9d27760f94f
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-03-31 16:54:23 +0530

    code: create component for root route
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,10 +2,14 @@ import { Toaster } from "@/components/ui/sonner";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
     <div className="flex select-none min-h-screen justify-center px-3 py-4 md:p-4">
       <Outlet />
       <Toaster />
     </div>
-  ),
-});
+  );
+}
