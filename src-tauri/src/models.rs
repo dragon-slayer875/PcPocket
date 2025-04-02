c721e2ebd608d6705842dc60125f72f849a0db92
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-04-02 20:58:31 +0530

    code: update schema and models
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -4,17 +4,17 @@ use diesel::prelude::*;
 #[diesel(table_name = crate::schema::bookmarks_table)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Bookmark {
-    pub id: i32,
+    pub id: Option<i32>,
     pub title: String,
     pub link: String,
-    pub icon_link: String,
-    pub created_at: chrono::NaiveDateTime,
+    pub icon_link: Option<String>,
+    pub created_at: i32,
 }
 
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::schema::tags_table)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Tag {
-    pub bookmarks_id: i32,
+    pub bookmark_id: i32,
     pub tag_name: String,
 }
--- src-tauri/src/schema.rs
+++ src-tauri/src/schema.rs
@@ -1,17 +1,22 @@
+// @generated automatically by Diesel CLI.
+
 diesel::table! {
     bookmarks_table (id) {
-        id -> Integer,
+        id -> Nullable<Integer>,
         title -> Text,
         link -> Text,
-        icon_link -> Text,
-        created_at -> Timestamp,
+        icon_link -> Nullable<Text>,
+        created_at -> Integer,
     }
 }
 
 diesel::table! {
     tags_table (bookmark_id, tag_name) {
-        // table has a composite primary key made of bookmarks_id which is a foreign key and tag_name
         bookmark_id -> Integer,
         tag_name -> Text,
     }
 }
+
+diesel::joinable!(tags_table -> bookmarks_table (bookmark_id));
+
+diesel::allow_tables_to_appear_in_same_query!(bookmarks_table, tags_table,);
