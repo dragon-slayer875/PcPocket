f6ebe30a95ea447311f4c99e456885bf7f80f111
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-04-05 04:10:16 +0530

    code: remove the tauri sql plugin
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,4 @@
 use tauri::async_runtime::spawn;
-use tauri_plugin_sql::{Migration, MigrationKind};
 
 mod commands;
 mod database_cmds;
@@ -16,28 +15,6 @@ mod dock;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let migrations = vec![
-        // Define your migrations here
-        Migration {
-            version: 1,
-            description: "create_initial_tables",
-            sql: "CREATE TABLE bookmarks_table (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT NOT NULL,
-  link TEXT NOT NULL,
-  icon_link TEXT,
-  created_at INTEGER NOT NULL DEFAULT (strftime('%s','now'))
-);
-            CREATE TABLE tags_table (
-  bookmark_id INTEGER NOT NULL,
-  tag_name TEXT NOT NULL,
-  PRIMARY KEY (bookmark_id, tag_name),
-  FOREIGN KEY (bookmark_id) REFERENCES bookmarks_table(id) ON DELETE CASCADE
-);",
-            kind: MigrationKind::Up,
-        },
-    ];
-
     let mut builder = tauri::Builder::default();
     #[cfg(desktop)]
     {
@@ -74,11 +51,6 @@ pub fn run() {
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_fs::init())
-        .plugin(
-            tauri_plugin_sql::Builder::default()
-                .add_migrations("sqlite:bookmarks.tmp", migrations)
-                .build(),
-        )
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_deep_link::init())
         .setup(|app| {
