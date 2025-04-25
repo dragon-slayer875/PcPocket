ebdbdfcea71458ab33d2f49474b58140366a57d5
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2025-04-26 05:18:59 +0530

    chore: add router devtools
    Signed-off-by: Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/appSidebar";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
@@ -16,6 +17,7 @@ function RootComponent() {
       <SidebarProvider defaultOpen={false}>
         <AppSidebar />
         <Outlet />
+        <TanStackRouterDevtools />
         <Toaster />
       </SidebarProvider>
     </div>
